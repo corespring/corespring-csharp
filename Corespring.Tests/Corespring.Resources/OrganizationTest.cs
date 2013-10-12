3d06480528bc4ae95875680cd8352b6137188dc1
benburton <benjamin.joseph.burton@gmail.com>
2013-10-12 09:30:33 -0400

    Use soft tabs

--- Corespring.Tests/Corespring.Resources/OrganizationTest.cs
+++ Corespring.Tests/Corespring.Resources/OrganizationTest.cs
@@ -7,21 +7,20 @@ using System.Collections.Generic;
 
 namespace Corespring.Resources.Tests
 {
-	[TestFixture]
-	public class OrganizationTest
-	{
-		[Test]
-		public void testOrganizationSerialization()
-		{
-			Organization organization = new Organization("id", "name", new List<string>(), false);
-			MemoryStream stream = new MemoryStream();
-			DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Organization));
-			serializer.WriteObject(stream, organization);
+    [TestFixture]
+    public class OrganizationTest
+    {
+        [Test]
+        public void testOrganizationSerialization()
+        {
+            Organization organization = new Organization("id", "name", new List<string>(), false);
+            MemoryStream stream = new MemoryStream();
+            DataContractJsonSerializer serializer = new DataContractJsonSerializer(typeof(Organization));
+            serializer.WriteObject(stream, organization);
 
-			stream.Position = 0;
-			StreamReader streamReader = new StreamReader(stream);
-			Assert.AreEqual("{\"id\":\"id\",\"isRoot\":false,\"name\":\"name\",\"path\":[]}", streamReader.ReadToEnd());
-		}
-	}
+            stream.Position = 0;
+            StreamReader streamReader = new StreamReader(stream);
+            Assert.AreEqual("{\"id\":\"id\",\"isRoot\":false,\"name\":\"name\",\"path\":[]}", streamReader.ReadToEnd());
+        }
+    }
 }
-
--- Corespring/Corespring.Resources/Organization.cs
+++ Corespring/Corespring.Resources/Organization.cs
@@ -4,28 +4,27 @@ using System.Collections.Generic;
 
 namespace Corespring.Resources
 {
-	[DataContract]
-	public class Organization
-	{
-		[DataMember(Name = "id")]
-		public string id { get; set; }
+    [DataContract]
+    public class Organization
+    {
+        [DataMember(Name = "id")]
+        public string id { get; set; }
 
-		[DataMember(Name = "name")]
-		public string name { get; set; }
+        [DataMember(Name = "name")]
+        public string name { get; set; }
 
-		[DataMember(Name = "path")]
-		public ICollection<string> path;
+        [DataMember(Name = "path")]
+        public ICollection<string> path;
 
-		[DataMember(Name = "isRoot")]
-		public bool root { get; set; }
+        [DataMember(Name = "isRoot")]
+        public bool root { get; set; }
 
-		public Organization(string id, string name, ICollection<string> path, bool root)
-		{
-			this.id = id;
-			this.name = name;
-			this.path = path;
-			this.root = root;
-		}
-	}
+        public Organization(string id, string name, ICollection<string> path, bool root)
+        {
+            this.id = id;
+            this.name = name;
+            this.path = path;
+            this.root = root;
+        }
+    }
 }
-
